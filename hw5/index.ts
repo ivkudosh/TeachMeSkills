569e820902be1fcce280ece80b88b96810283a9e
ivkud <ivkudosh@gmail.com>
2023-02-14 21:20:14 +0300

    fix: fixed task 2: advanced - line 100

--- hw5/index.ts
+++ hw5/index.ts
@@ -97,7 +97,7 @@ const breakChocolate = (n: number, m: number): number => {
     if (n === 0 || m === 0 || (n === 1 && m === 1)) {
         return 0;
     } else {
-        return n * (m - 1);
+        return n * m - 1;
     }
 }
 
