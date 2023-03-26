af369328ef30210438fb856e74bbe60ec8f47259
ivkud <ivkudosh@gmail.com>
2023-03-26 20:56:44 +0300

    fix: added default

--- hw16/calculator.ts
+++ hw16/calculator.ts
@@ -12,5 +12,7 @@ export const calculator = (num1: number, num2: number, operator: Operators) => {
             return num1 * num2;
         case "^":
             return num1 ** num2;
+        default:
+            throw new Error('Invalid operation');
     }
 };
