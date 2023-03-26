71ca2fdb1b46816395caa0c98dba2da9eadc3fcb
ivkud <ivkudosh@gmail.com>
2023-03-26 20:57:01 +0300

    fix: changed negative tests

--- hw16/index.ts
+++ hw16/index.ts
@@ -10,10 +10,10 @@ checker(99, 1, "+", 100);
 checker(1, 0, "-", 1);
 
 // Negative tests
-checker(2, 3, "^", 10);
-checker(-1, 3, "+", 100);
+checker(1, 100, "^", 10);
+checker(-1, 0, "+", 100);
 checker(0, 1, "-", 1);
-checker(50, 3, "*", -150);
-checker(-8, 4, "/", 2);
+checker(1, 1, "*", -100);
+checker(-100, -100, "/", 0);
 checker(1, 100, "-", 99);
 checker(100, 0, "*", 100);
