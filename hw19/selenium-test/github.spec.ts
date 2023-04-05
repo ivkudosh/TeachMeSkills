9d93332c608fa887dd3eb3011c33779d85730714
ivkud <ivkudosh@gmail.com>
2023-04-05 16:47:36 +0300

    fix: fixed new comments

--- hw19/selenium-test/github.spec.ts
+++ hw19/selenium-test/github.spec.ts
@@ -3,15 +3,15 @@ import { By, until, Builder, Capabilities, Key } from "selenium-webdriver";
 import { getRandomNameGitHub } from "../random";
 
 const baseUrl = "https://github.com";
-const loginButton = '//a[@href="/login"]';
-const priceButton = 'header a[href="/pricing"]';
+const loginButtonLocator = '//a[@href="/login"]';
+const priceButtonLocator = 'header a[href="/pricing"]';
 const searchInputLocator = 'input[data-unscoped-placeholder="Search GitHub"]';
 
 const nameGitHub = getRandomNameGitHub();
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build();
 
 enum Plan {
-    Free = "Free"
+    FREE = "Free"
 }
 
 describe("GitHub tests", () => {
@@ -32,22 +32,22 @@ describe("GitHub tests", () => {
     });
 
     it(`Should have 'Sign In' button`, async () => {
-        await driver.findElement(By.xpath(loginButton)).click();
+        await driver.findElement(By.xpath(loginButtonLocator)).click();
         await driver.wait(until.urlIs(`${baseUrl}/login`));
     });
 
     it('Should be "Incorrect username or password." text after sign in with empty fields: Username or email address, Password', async () => {
-        await driver.findElement(By.xpath(loginButton)).click();
+        await driver.findElement(By.xpath(loginButtonLocator)).click();
         await driver.wait(until.elementLocated(By.css('input[value="Sign in"]'))).click();
-        const text = await driver.wait(until.elementLocated(By.css('div[class="flash flash-full flash-error  "] div[aria-atomic="true"]'))).getText();
-        expect(text).to.be.equal("Incorrect username or password.");
+        const invalidCredentialsMessage = await driver.wait(until.elementLocated(By.css('div[class="flash flash-full flash-error  "] div[aria-atomic="true"]'))).getText();
+        expect(invalidCredentialsMessage).to.be.equal("Incorrect username or password.");
     });
 
-    it(`Should be ${Plan.Free} text on the free plan`, async () => {
-        await driver.findElement(By.css(priceButton)).click();
-        const text = await driver.wait(until.elementLocated(By.css('div[data-min-seats="1"] h2[class="mb-2 h5-mktg"]')));
-        const checkText = await text.getText();
-        expect(checkText).to.be.equal(Plan.Free);
+    it(`Should be ${Plan.FREE} text on the free plan`, async () => {
+        await driver.findElement(By.css(priceButtonLocator)).click();
+        const freePlanTitleElement = await driver.wait(until.elementLocated(By.css('div[data-min-seats="1"] h2[class="mb-2 h5-mktg"]')));
+        const freePlanTitleText = await freePlanTitleElement.getText();
+        expect(freePlanTitleText).to.be.equal(Plan.FREE);
     });
 
     it('Should search by name', async () => {
