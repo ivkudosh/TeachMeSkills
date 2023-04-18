a212178ac0e6809c03da69b861b13389f47e8393
ivkud <ivkudosh@gmail.com>
2023-04-18 16:57:22 +0300

    fix: used pagFactory

--- hw22/src/step-definitions/common-steps.ts
+++ hw22/src/step-definitions/common-steps.ts
@@ -1,46 +1,28 @@
 import { Given, Then } from "@cucumber/cucumber";
 import { Pages } from "../helpers/types";
 import { PageFactory } from "../pageObjects/pageFactory";
-import { homePage } from "../pageObjects/homePage";
-import { pricingPage } from "../pageObjects/pricingPage";
-import { searchPage } from "../pageObjects/searchPage";
 import { BASE_URL } from "../helpers/constants";
 import { randomName } from "../helpers/constants";
 import { expect } from "chai";
-import { signInPage } from "../pageObjects/signInPage";
-
-
 
 Given(/^the user opens "(.+)" page via link$/, async (pageName: Pages) => {
     await PageFactory.getPage(pageName).visitPage();
 });
 
-Then(/^the user is on "Home" page$/, async () => {
-    await homePage.waitForPage();
+Then(/^the user is on "(.+)" page$/, async (pageName: Pages) => {
+    await PageFactory.getPage(pageName).waitForPage();
 });
 
-Then(/^the user clicks Enter on keyboard on "Home" page$/, async () => {
-    await homePage.pressEnter();
+Then(/^the user clicks Enter on keyboard on "(.+)" page$/, async (pageName: Pages) => {
+    await PageFactory.getPage(pageName).pressEnter();
 });
 
-Then(/^the user is on "Pricing" page$/, async () => {
-    await pricingPage.waitForPage();
-});
-
-Then(/^the user is on "Search" page$/, async () => {
-    await searchPage.waitForPage();
-});
-
-Then(/^the user sees that url of the "Search" page with random name$/, async () => {
-    const actualUrl = await searchPage.getCurrentUrl();
+Then(/^the user sees that url of the "(.+)" page with random name$/, async (pageName: Pages) => {
+    const actualUrl = await PageFactory.getPage(pageName).getCurrentUrl();
     expect(actualUrl).to.be.include(`${BASE_URL}/search?q=${randomName}`);
 });
 
-Then(/^the user is on "Sign In" page$/, async () => {
-    await signInPage.waitForPage();
-});
-
-Then(/^the user sees that url of the "Sign In" page is "(.+)"$/, async (expectedUrl: string) => {
-    const actualUrl = await signInPage.getCurrentUrl();
+Then(/^the user sees that url of the "(.+)" page is "(.+)"$/, async (pageName: Pages, expectedUrl: string) => {
+    const actualUrl = await PageFactory.getPage(pageName).getCurrentUrl();
     expect(actualUrl).to.be.equal(expectedUrl);
 });
