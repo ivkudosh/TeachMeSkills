ce1e12f38495d0c9df33b2cbdb4b0ce5725815ea
ivkud <ivkudosh@gmail.com>
2023-05-04 13:33:28 +0300

    fix: added logs

--- hw22/src/random.ts
+++
@@ -1,6 +0,0 @@
-export const getRandomNameGitHub = () => {
-    const names: string[] = ['Ilya', 'Roma', 'Ivan', "Vasiliy", "Jack", "Pasha", "Artur", "Andrei", "David", "Peter", "Alex", "Yan", "Vlad", "Vadim"];
-    const randName = Math.floor(Math.random() * names.length);
-    const nameValue = names[randName];
-    return nameValue;
-};
