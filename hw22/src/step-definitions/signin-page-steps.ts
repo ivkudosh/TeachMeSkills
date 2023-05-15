327c18b60361213ecee39c2aa6d5900da7eec75e
ivkud <ivkudosh@gmail.com>
2023-05-15 15:03:16 +0300

    fix: fixed all steps

--- hw22/src/step-definitions/signin-page-steps.ts
+++ hw22/src/step-definitions/signin-page-steps.ts
@@ -9,7 +9,7 @@ Then(/^the user clicks on "Sign In" input on "Sign In" page$/, async () => {
     await signInPage.clickOnSignInInput();
 });
 
-Then(/^the user sees flash error with text "Sign In" on "Sign In" page$/, async (expectedText: string) => {
+Then(/^the user sees flash error with text "(.+)" on "Sign In" page$/, async (expectedText: string) => {
     logger.info(`Getting flash error with text ${expectedText} on ${Pages.SIGN_IN} page...`);
     const actualText = await signInPage.getFlashErrorLabelText();
     logger.info(`Flash error with text ${expectedText} on ${Pages.SIGN_IN} page...`);
